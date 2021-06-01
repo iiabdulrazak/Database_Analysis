e32309db9a3f33a4f68107331832b04a04b65772
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-06-01 15:35:05 +0300

    removing environment declaration

--- dataExtracting.py
+++ dataExtracting.py
@@ -8,11 +8,7 @@ try:
   import matplotlib_inline
 except Exception as e:
   print(f'Error while implementing!\n {e}')
-
-#getting environment variables
-dbUser = os.getenv('testUserDB')
-password = os.environ.get('testPassDB')
-
+  
 #creating connection
 dbConnection = conn.connect(host='sql6.freesqldatabase.com',
                             user='sql6410935', passwd='GHh4XfMTfT',
