62bfcddd24528d406eeec44325551c17e3bb2fdd
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-06-01 15:29:33 +0300

    creating connection

---
+++ dataExtracting.py
@@ -0,0 +1,17 @@
+try:
+  import os
+  import numpy
+  import pandas as pd
+  import seaborn as sns
+  import matplotlib.pyplot as plt
+  import mysql.connector as conn
+  import matplotlib_inline
+except Exception as e:
+  print(f'Error while implementing!\n {e}')
+
+#getting environment variables
+dbUser = os.getenv('testUserDB')
+password = os.environ.get('testPassDB')
+
+#creating connection
+dbConnection = conn.connect(host='sql6.freesqldatabase.com', user=dbUser, passwd=password, db='sql6410935', port=3306)
