32a8c1e05a3227eb8056338f49a632f9858c5413
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-06-01 23:00:24 +0300

    stable script v0.1

--- dataExtracting.py
+++ dataExtracting.py
@@ -20,5 +20,8 @@ dbConnection = conn.connect(host='sql6.freesqldatabase.com',
 print(f'Connection ... \n{dbConnection}')
 
 #lets get some data from the database
-corona_tables = pd.read_sql_query('DESCRIBE corona', dbConnection)
-print(corona_tables)
+corona_show = pd.read_sql_query('SHOW TABLES FROM sql6410935', dbConnection)
+corona_describe = pd.read_sql_query('DESCRIBE corona', dbConnection)
+corona_select = pd.read_sql_query('SELECT * FROM corona', dbConnection)
+
+print(f'Show all Tables:\n{corona_show} \n\n Describe Table:\n{corona_describe} \n\n Select all Data:\n{corona_select}')
