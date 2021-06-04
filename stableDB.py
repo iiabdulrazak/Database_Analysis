22bf2363ff0ddf189a8b51ef5651a30e74fb21d7
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-06-04 22:41:43 +0300

    testing new methods

--- stableDB.py
+++ stableDB.py
@@ -1,13 +1,16 @@
 try:
-  import pandas as pd
-  import mysql.connector as conn
+    import re
+    import pandas as pd
+    import mysql.connector as conn
 except Exception as e:
-  print(f'Error while implementing!\n {e}')
+    print(f'Error while implementing!\n {e}')
 
 #creating connection
 dbConnection = conn.connect(host='34.87.39.39',
                             user='root', passwd='Dz(_11@HeR#VL',
                             db='corona', port=3306)
+#init the cursor
+cursor = dbConnection.cursor()
 #check if connection established!
 print(f'Connection ... \n{dbConnection}\n')
 
@@ -19,16 +22,23 @@ createQuery = '''
 checkQuery = '''
                 SELECT * FROM coron
              '''
-
+             
 insertQuery = '''
-                LOAD DATA INFILE '/home/liquidx/Desktop/sarcasmDet_Database_Analysis/data/data.csv' 
-                INTO TABLE corona
-                FIELDS TERMINATED BY ',' 
-                ENCLOSED BY '"'
-                LINES TERMINATED BY '\n'
-                IGNORE 1 ROWS;
-              '''
+                INSERT INTO corona(id,date,country,confirmed,recovered,deaths) VALUES(%s,%s,%s,%s,%s,%s)
+             '''
+             
+#creating variable out of csv file
+data = pd.read_csv('data/data.csv')
+ids = data.index
+date = data['date']
+country = data['country']
+confirmed = data['confirmed']
+recovered = data['recovered']
+deaths = data['deaths']
 
-#lets get some data from the database
-data = pd.read_sql_query(insertQuery, dbConnection)
-print(f'Table created...\n{data}')
+allVariables = (date,country,confirmed,recovered,deaths)
+cursor.execute(insertQuery, allVariables)
+dbConnection.commit()
+
+#lets print the output from the database
+print(checkQuery, dbConnection)
